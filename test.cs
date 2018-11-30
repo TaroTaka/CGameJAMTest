b4a57a0fa0fc7bbb9f04d14e72822464abd7b150
TaroTaka <lokkehokke@gmail.com>
2018-11-30 23:39:44 +0900

    Test

--- test.cs
+++ test.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class test : MonoBehaviour {
 
-	// Use this for initialization
 	void Start () {
 
 	}
 
-	// Update is called once per frame
 	void Update () {
 
 	}
-	
+
 }
